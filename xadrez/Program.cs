46dec029475f477a9b3f7b83bf013b07f7a3416a
Jygoulas <nycolasmoura41@gmail.com>
2017-12-11 15:26:44 -0200

    Melhorando  Imprimir Tela

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -8,12 +8,26 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 2));
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(3, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (tabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
+
+
+        
     }
 }
